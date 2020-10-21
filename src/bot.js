77284c1e1264f44930da5a05340ff5033d91ab21
Eduardo Maciel <eduardomacielbr@gmail.com>
2020-10-21 20:56:42 -0300

    Implementação do express no bot

--- src/bot.js
+++ src/bot.js
@@ -3,6 +3,21 @@ const Discord = require('discord.js');
 const { registerCommands, registerEvents } = require('./utils/registry');
 const client = new Discord.Client();
 
+const express = require('express');
+let bodyParser = require('body-parser');
+const app = express();
+
+async function startApp() {
+    let promise = client.login(token)
+    promise.catch(function(error) {
+      console.error("Discord bot login | " + error);
+    });
+}
+startApp();
+client.on("ready", () => {
+  console.log("Successfully logged in Discord bot.");
+})
+
 client.queues = new Map();
 
 (async () => {
@@ -11,3 +26,18 @@ client.queues = new Map();
     await registerEvents(client, '../events');
     await client.login(process.env.TOKEN);
 })();
+
+app.use(express.static('public'));
+
+app.get('/', function(request, response) {
+  if (request.headers.username != undefined) { 
+      const channel = client.channels.get(request.headers.cid);
+      if (request.headers.rblxerror == undefined) {
+        channel.send('Successfully ' + request.headers.method + 'ned user ' + request.headers.username + " | ID: " + request.headers.value);
+      } else {
+        channel.send("Failed to " + request.headers.method + " user: " + request.headers.username + " | ID: " + request.headers.value + " | `Rblx-Error:  " + request.headers.rblxerror + "`"); 
+      }
+  }
+  response.send(toBan[0]);
+  toBan.shift();
+});
